306cdf5cae840a92b8bb2798d10fa9555371929c
urvishd <push.default>
2020-08-08 22:49:09 +0530

    lab2

---
+++ LAB_2/addition.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2_classlibrary
+{
+    public class addition
+    {
+        public int add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ LAB_2/multiplication.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2_classlibrary
+{
+    public class multiplication
+    {
+        public int multiply(int x,int y)
+        {
+            return x * y;
+        }
+    }
+}
