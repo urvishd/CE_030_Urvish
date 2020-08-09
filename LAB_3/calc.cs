bc69f2c384923ba7075af4b7ed41c99beaa5419b
urvishd <push.default>
2020-08-09 09:28:57 +0530

    lab3

---
+++ LAB_3/calc.cs
@@ -0,0 +1,15 @@
+using System;
+namespace prog
+{
+	public class calculator
+	{
+		public int addition(int x,int y)
+		{
+			return x+y;
+		}
+	public int multiplication(int x,int y)
+		{
+			return x*y;
+		}
+	}
+}
---
+++ LAB_3/calc_new.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+
+[assembly:AssemblyVersion("1.0.0.0")]
+namespace prog
+{
+	public class calculator
+	{
+		public int addition(int x,int y)
+		{
+			return x+y;
+		}
+	public int multiplication(int x,int y)
+		{
+			return x*y;
+		}
+	}
+}
